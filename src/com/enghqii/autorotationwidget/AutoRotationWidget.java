365249d22daa26d5bad6a16237886e5a44a68950
Chanwoo Jeong <enghqii@gmail.com>
2014-08-23 23:42:01 +0900

    Fixed untracked files

--- src/com/enghqii/autorotationwidget/AutoRotationWidget.java
+++ src/com/enghqii/autorotationwidget/AutoRotationWidget.java
@@ -1,25 +1,51 @@
 package com.enghqii.autorotationwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class AutoRotationWidget extends AppWidgetProvider {
 
+	public static final String ACTION_EVENT = "com.enghqii.widget.ACTION_EVENT";
+
 	public AutoRotationWidget() {
-		
+
 	}
 
 	@Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
- 
-        RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.widget_design);
-        remoteView.setTextViewText(R.id.title, "Widget test");
-        appWidgetManager.updateAppWidget(appWidgetIds, remoteView);
-        
-        Toast.makeText(context, "widget clicked", Toast.LENGTH_SHORT).show();
-    }
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+
+		String action = intent.getAction();
+
+		if (ACTION_EVENT.equals(action)) {
+			this.onButtonClick(context);
+		}
+	}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+		RemoteViews remoteView = new RemoteViews(context.getPackageName(),
+				R.layout.widget_design);
+
+		// let 'button1' send pendingIntent
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0,
+				new Intent(ACTION_EVENT), 0);
+		remoteView.setOnClickPendingIntent(R.id.button1, pendingIntent);
+
+		appWidgetManager.updateAppWidget(appWidgetIds, remoteView);
+	}
+
+	private void onButtonClick(Context context) {
+
+		Toast.makeText(context, "Do what i want", Toast.LENGTH_SHORT).show();
+	}
 }
